139e7b496f7a2deb6773633b5928efdb17b1f297
ErnestGod <e.god465@gmail.com>
2018-10-17 01:00:44 +0200

    Constrains and things toDo

--- Bitcoin Picker/ViewController.swift
+++ Bitcoin Picker/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    //TODO:
+    
+    //Pre-setup IBOutlets
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+        //TODO:
+    
+        //MARK: - JSON Parsing
+        /********************/
+    
+        //MARK: - Networking
+        /*******************/
+    
 
 }
 
